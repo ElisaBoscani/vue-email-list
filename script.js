06a98b9e9a46b0a5f9a202a3a9b785e863ed8cc9
Elisa Boscani <elisabosc90@gmail.com>
2023-09-20 15:44:39 +0200

    creato un array con 10 email e messe in paggina

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ createApp({
   data() {
     return {
       message: "Hello Vue!",
-      email: [],
+      emails: [],
     };
   },
   mounted() {
@@ -12,7 +12,11 @@ createApp({
       .get("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => {
         console.log(response);
-        this.email = response.data.response;
+        for (let index = 0; index < 10; index++) {
+          this.emails[index] = response.data.response;
+        }
+
+        console.log(this.emails);
       });
   },
 }).mount("#app");
