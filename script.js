4366a4f6422a1b442e831e7bba8b3d0b0ff1bedc
Elisa Boscani <elisabosc90@gmail.com>
2023-09-20 16:45:02 +0200

    create le email in maniera dinamica richiamando sempre api e sistemato un po lo stile

--- script.js
+++ script.js
@@ -7,16 +7,22 @@ createApp({
       emails: [],
     };
   },
+  methods: {
+    getColor(index) {
+      const color = ["red", "blue", "green", "yellow", "pink"];
+      return color[index % color.length];
+    },
+  },
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((response) => {
-        console.log(response);
-        for (let index = 0; index < 10; index++) {
-          this.emails[index] = response.data.response;
-        }
+    for (let index = 0; index < 10; index++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          console.log(response);
 
+          this.emails[index] = response.data.response;
+        }),
         console.log(this.emails);
-      });
+    }
   },
 }).mount("#app");
